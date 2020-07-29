35bd1d6dc2f1e5dd2f846b13cec219a51a2d3ade
angelrojasm <angelrojasm6@gmail.com>
2020-07-29 19:30:54 -0400

    Fixed server to send static build on deployment

--- packages/server/app.js
+++ packages/server/app.js
@@ -7,15 +7,26 @@ const sampleModel = require('./mongoose/sampleModel');
 app.use(express.json());
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	app.use(express.static('../client/build'));
+
+	// Express serve up index.html file if it doesn't recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+app.listen(process.env.PORT || port, () => {
+	console.log('app is running on port ' + port);
+});
+
 app.get('/hello', function (req, res) {
 	res.send('hello from Express!');
 });
 
 app.get('/get-all', async function (req, res) {
 	res.send(await sampleModel.find());
-});
-app.listen(process.env.PORT || port, () => {
-	console.log('app is running on port ' + port);
 });
 
 module.exports = app;
