b0df378882c59d9b3e0cb7c006c93164487da351
angelrojasm <angelrojasm6@gmail.com>
2020-07-29 21:00:04 -0400

    Removed unnecessary code

--- packages/server/app.js
+++ packages/server/app.js
@@ -8,14 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-	// Express will serve up production assets
 	app.use(express.static('../client/build'));
-
-	// Express serve up index.html file if it doesn't recognize route
-	/*const path = require('path');
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-	});*/
 }
 app.listen(process.env.PORT || port, () => {
 	console.log('app is running on port ' + port);
